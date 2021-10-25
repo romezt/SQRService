96721d7675aedaa473e653bab4bb8c2a9f3696d5
Anton <ikkii780@mail.ru>
2021-10-25 12:26:30 +0500

    sqr2

--- main/java/ru/netology/sqr/SQRService.java
+++ main/java/ru/netology/sqr/SQRService.java
@@ -4,7 +4,6 @@ public class SQRService {
 
     public int sqrRange(int numOne, int numTwo) {
         int count = 0;
-        int j;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= numOne && i * i <= numTwo) {
                 count = count + 1;
